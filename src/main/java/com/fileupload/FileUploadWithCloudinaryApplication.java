ad48d5cdaccdd44f24b71717ba9ee2ea7449d277
krtuhin <tuhin.km2774@gmail.com>
2023-08-29 01:01:20 +0530

    Project Created

---
+++ src/main/java/com/fileupload/FileUploadWithCloudinaryApplication.java
@@ -0,0 +1,13 @@
+package com.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadWithCloudinaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadWithCloudinaryApplication.class, args);
+    }
+
+}
